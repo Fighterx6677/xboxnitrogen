13f1e797ce40115897489c1d40f172408d41e28b
Fighterx6677 <74173478+Fighterx6677@users.noreply.github.com>
2021-01-25 17:04:38 +0530

    Add files via upload

---
+++ xbox nitro gen/application ( if you dont have python )/readme.txt
@@ -0,0 +1,9 @@
+this project is not meant to be copywrited uploaded without credits and my linkvertise download only
+
+how to use this project?
+
+it will generate unchecked xbox nitro codes which can be checked in future
+
+this project is made by fighter x official yt 
+
+you must have python to run the python file if you dont have python you can run it with the application on application folder
---
+++ xbox nitro gen/main.py
@@ -0,0 +1,80 @@
+#imports
+
+import colorama
+import random
+import os
+import requests
+import time
+
+#start-engine
+
+colorama.init()
+os.system('cls')
+print("welcome to xbox nitro generator"); time.sleep(2)
+os.system('cls')
+
+#hot-keys
+
+def Clear():
+    os.system('cls')
+
+def Interval():
+    time.sleep(10)
+
+#main-code
+
+def logo():
+    print("""
+    
+██╗░░██╗██████╗░░█████╗░██╗░░██╗  ███╗░░██╗██╗████████╗██████╗░░█████╗░  ░██████╗░███████╗███╗░░██╗
+╚██╗██╔╝██╔══██╗██╔══██╗╚██╗██╔╝  ████╗░██║██║╚══██╔══╝██╔══██╗██╔══██╗  ██╔════╝░██╔════╝████╗░██║
+░╚███╔╝░██████╦╝██║░░██║░╚███╔╝░  ██╔██╗██║██║░░░██║░░░██████╔╝██║░░██║  ██║░░██╗░█████╗░░██╔██╗██║
+░██╔██╗░██╔══██╗██║░░██║░██╔██╗░  ██║╚████║██║░░░██║░░░██╔══██╗██║░░██║  ██║░░╚██╗██╔══╝░░██║╚████║
+██╔╝╚██╗██████╦╝╚█████╔╝██╔╝╚██╗  ██║░╚███║██║░░░██║░░░██║░░██║╚█████╔╝  ╚██████╔╝███████╗██║░╚███║
+╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚═╝░░╚══╝╚═╝░░░╚═╝░░░╚═╝░░╚═╝░╚════╝░  ░╚═════╝░╚══════╝╚═╝░░╚══╝
+                                       made by fighter x""")
+    
+    print("""
+    [1] generate codes
+    [2] check out makers youtube
+    [3] instructions
+    [4] exit
+    """)
+    choice = int(input("[?]: "))
+    if choice == 1:
+        codeLen = 24
+        letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+        lp = int(input("Enter The Number Of Unchecked xbox nitros You Need: "))
+        k = open('xboxcodes.txt', 'w')
+        for i in range(lp):
+            k.write("https://discord.com/billing/promotions/xbox-game-pass/redeem/" + ''.join(random.choice(letters) for i in range(codeLen)) + '\n')
+        k.close()
+        Clear()
+        logo()
+    
+    elif choice == 2:
+        r = requests.get('http://youtube.com/c/fighterxofficialyt')
+        print(r.url + " --> subscribe now!!!")
+        Interval()
+        Clear()
+        logo()
+
+    elif choice == 3:
+        os.system('type readme.txt')
+        Interval()
+        Clear()
+        logo()
+
+    elif choice == 4:
+        x = 4
+
+#program-starter
+
+logo()
+
+
+
+    
+
+    
+
